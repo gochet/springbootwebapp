56ef89ee378af72669ac552358e3476306939aad
Chet Kuhn <gochet@gmail.com>
2016-04-11 18:37:41 -0500

    solved h2 console issue, began transition to postgresql.

--- src/main/java/guru/springframework/bootstrap/ProductLoader.java
+++ src/main/java/guru/springframework/bootstrap/ProductLoader.java
@@ -26,7 +26,7 @@ public class ProductLoader implements ApplicationListener<ContextRefreshedEvent>
     public void onApplicationEvent(ContextRefreshedEvent event) {
 
         Product shirt = new Product();
-        shirt.setDescription("Spring Framework Guru Shirt");
+        shirt.setDescription("Chet's Famous Shirt");
         shirt.setPrice(new BigDecimal("18.95"));
         shirt.setImageUrl("https://springframework.guru/wp-content/uploads/2015/04/spring_framework_guru_shirt-rf412049699c14ba5b68bb1c09182bfa2_8nax2_512.jpg");
         shirt.setProductId("235268845711068308");
@@ -35,7 +35,7 @@ public class ProductLoader implements ApplicationListener<ContextRefreshedEvent>
         log.info("Saved Shirt - id: " + shirt.getId());
 
         Product mug = new Product();
-        mug.setDescription("Spring Framework Guru Mug");
+        mug.setDescription("Chet's Famous Mug");
         mug.setImageUrl("https://springframework.guru/wp-content/uploads/2015/04/spring_framework_guru_coffee_mug-r11e7694903c348e1a667dfd2f1474d95_x7j54_8byvr_512.jpg");
         mug.setProductId("168639393495335947");
         mug.setPrice(new BigDecimal("11.95"));
